3b722e234b9eb2a7350746b9cf1d9a209f3a4858
Arnav85 <160046642+Arnav85@users.noreply.github.com>
2024-05-23 20:21:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let name = "Arnav" //string datatype
+let age = 22 //number datatype
+let isloggedin = true/false //boolean
+
+// number = 2 to the power of 53
+//bigint
+//string =>""
+//boolean = true/false
+//null = standalone value; (null ka type hai object but undefined ka type undefined hi hota hai)
+// undefined =>
+//symbol =>unique
+//objects
+console.log(typeof "Arnav")
