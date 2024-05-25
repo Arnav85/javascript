5213ef0a1de9ee59e4cf705f872c68c168dce880
Arnav85 <160046642+Arnav85@users.noreply.github.com>
2024-05-25 08:48:56 +0000

    conversionsofDatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+/*let score = "33abcd"
+console.log(typeof score);
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+let isLoggedin =1
+let booleanIsLoggedin = Boolean(isLoggedin)
+console.log(isLoggedin);
