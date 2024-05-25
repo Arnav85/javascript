95b5925334b04789b3191308e7ef1122a3d17a9e
Arnav85 <160046642+Arnav85@users.noreply.github.com>
2024-05-25 14:03:37 +0000

    Comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,6 +5,14 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);*/
 
-let isLoggedin =1
-let booleanIsLoggedin = Boolean(isLoggedin)
-console.log(isLoggedin);
+//let isLoggedin =1
+//let booleanIsLoggedin = Boolean(isLoggedin)
+//console.log(isLoggedin);
+
+//****** CONVERSION OPERATIONS*********/
+
+console.log(2+2)
+console.log("2"+2)
+console.log(2+"2")
+console.log(2+1+"2")
+console.log("2"+1+2)
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log("2">1);
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+/* The reason is that a equality checker == and comparison >,<,>=,<= work differently
+Comparison convert null to a number treating is as a 0
+thats why (null>=0) is true and rest of two is false.*/
+
+// === (strict check)
+console.log("2"===2)
