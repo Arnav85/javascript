f89844075e9f4afef7c3ff28b40a1c678d999601
Arnav85 <160046642+Arnav85@users.noreply.github.com>
2024-06-27 08:40:15 +0000

    iterations

---
+++ 05_Iterations/one.js
@@ -0,0 +1,31 @@
+// for 
+
+
+ /*for(let i = 0;i<=10;i++){
+    const element = i;
+    console.log(element);
+ }*/
+
+ // while and do-while loop
+
+ /*let index = 0
+ while(index <=10){
+    console.log(`value of index is ${index}`)
+    index = index+2;
+ }*/
+
+ let myarr = ["batman","spiderman","superman"]
+ let arr = 0
+ while(arr<myarr.length){
+    console.log(`value of array is ${myarr[arr]}`);
+    arr = arr+1;
+ }
+
+ // dowhile loop
+
+ let score = 1
+ do{
+    console.log(`score is ${score}`);
+    score++;
+ }
+ while(score <=10)
